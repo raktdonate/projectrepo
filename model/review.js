7ed2dd92b73f21cd7aea6c00071839627743a68c
khuban <khubanzehra@gmail.com>
2023-02-10 23:30:34 +0530

    chnaged whole designings

---
+++ model/review.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose")
+const Schema=mongoose.Schema
+const reviewSchema= new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    text:{
+        type:String,
+        required:true
+    }
+})
+module.exports=mongoose.model('Review',reviewSchema)
